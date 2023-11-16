97b269c52fc20d8a3d6d2b1ae6da87cfa958d0e9
AlessandroS01 <seghini53@gmail.com>
2023-11-16 17:02:18 +0100

    new navbar

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -2,7 +2,7 @@ window.addEventListener("scroll", function () {
   var sc = window.scrollY;
   var mainNavbar = document.getElementById("main-navbar");
 
-  if (sc > 100) {
+  if (sc > 60) {
     mainNavbar.classList.add("navbar-scroll");
   } else {
     mainNavbar.classList.remove("navbar-scroll");
