d1ec66c07304b795e861028c096bdebe67f65eac
AlessandroS01 <seghini53@gmail.com>
2023-11-25 12:06:25 +0100

    About page displaying members correctly. It remains to change the data retrieved after users modify the selected language

--- src/app/components/header/switchlang/switch-lang.component.ts
+++ src/app/components/header/switchlang/switch-lang.component.ts
@@ -9,16 +9,12 @@ import {LanguageService} from "../../../shared/services/language/language.servic
 })
 export class SwitchLangComponent implements OnInit{
 
-  isDivHidden: boolean = true;
-  langSelected: string = "English";
-  flagLangSelected: string = "assets/img/languages/united-kingdom.png";
+  // isDivHidden: boolean = true;
+  // langSelected: string = "English";
+  // flagLangSelected: string = "assets/img/languages/united-kingdom.png";
 
   ngOnInit() {
-    let langTag = document.getElementById('en');
-
-    if (langTag) {
-      langTag.classList.add("active");
-    }
+    this.changeDisplayCss('english-language');
   }
 
   constructor(
@@ -33,25 +29,54 @@ export class SwitchLangComponent implements OnInit{
     this.translate.use(lang);
 
     if (lang == "en") {
-      this.langSelected = "English"
-      this.flagLangSelected = "assets/img/languages/united-kingdom.png";
+      // this.langSelected = "English"
+      // this.flagLangSelected = "assets/img/languages/united-kingdom.png";
+      this.changeDisplayCss('english-language');
+
       this.languageService.setLanguage("en");
     }
     if (lang == "it") {
-      this.langSelected = "Italiano"
-      this.flagLangSelected = "assets/img/languages/italian.png";
+      // this.langSelected = "Italiano"
+      // this.flagLangSelected = "assets/img/languages/italian.png";
+      this.changeDisplayCss('italian-language');
       this.languageService.setLanguage("it");
     }
     if (lang == "ko") {
-      this.langSelected = "한국어"
-      this.flagLangSelected = "assets/img/languages/south-korea.png";
+      // this.langSelected = "한국어"
+      // this.flagLangSelected = "assets/img/languages/south-korea.png";
+      this.changeDisplayCss('korean-language');
       this.languageService.setLanguage("ko");
     }
 
-    this.isDivHidden = true;
+    //this.isDivHidden = true;
   }
 
+  /*
   toggleDiv() {
     this.isDivHidden = !this.isDivHidden;
+  }
+   */
+
+  changeDisplayCss(id: string) {
+    const element = document.getElementById(id);
+    if (element != null) {
+      element.classList.add('active');
+      this.removeDisplayCss(id);
+    }
+  }
+
+  removeDisplayCss(id: string) {
+    const allPElements = document.getElementsByTagName('p');
+
+    if (allPElements != null) {
+      for (let i = 0; i < allPElements.length; i++) {
+        const currentElement = allPElements[i];
+
+        if (currentElement.id !== id) {
+          // Remove the 'active' class from elements that were not clicked
+          currentElement.classList.remove('active');
+        }
+      }
+    }
   }
 }
